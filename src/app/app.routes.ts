7d98b408ef6bca42d5a9fcebe49b789934e583ed
LucaMimmo <171295375+LucaMimmo05@users.noreply.github.com>
2025-11-13 12:20:21 +0100

    Configure protected dashboard route with authentication guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,16 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
+import { Dashboard } from './dashboard/dashboard';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/register', pathMatch: 'full' },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'register', component: RegisterComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  {
+    path: 'dashboard',
+    component: Dashboard,
+    canActivate: [authGuard], // Route protetta
+  },
 ];
