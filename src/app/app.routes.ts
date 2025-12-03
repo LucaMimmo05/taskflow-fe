fcc8bafcc11245b217c7424cafd221044df548f0
LucaMimmo <171295375+LucaMimmo05@users.noreply.github.com>
2025-12-03 10:44:25 +0100

    Feature: implement guest guard for public pages and update routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,12 @@ import { LoginComponent } from './pages/login/login.component';
 import { Home } from './pages/home/home';
 import { ProjectsPage } from './pages/projects/projects';
 import { authGuard } from './guards/auth.guard';
+import { guestGuard } from './guards/guest.guard';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: 'register', component: RegisterComponent },
-  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'register', component: RegisterComponent, canActivate: [guestGuard] },
+  { path: 'login', component: LoginComponent, canActivate: [guestGuard] },
   { path: 'projects', component: ProjectsPage, canActivate: [authGuard] },
   {
     path: 'home',
